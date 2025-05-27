adc4d60d497907539e368f32beb7c3d86912ec7b
FANDESAWHOO <hcfpluginsv2@gmail.com>
2025-05-26 23:43:04 -0300

    Update FactionsAPI.java

--- src/main/java/org/hcgames/hcfactions/api/FactionsAPI.java
+++ src/main/java/org/hcgames/hcfactions/api/FactionsAPI.java
@@ -1,9 +1,34 @@
 package org.hcgames.hcfactions.api;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.hcgames.hcfactions.HCFactions;
+import org.hcgames.hcfactions.faction.Faction;
+import org.hcgames.hcfactions.structure.Relation;
+
+/**
+ * BETA
+ *
+ */
 public final class FactionsAPI {
 
+	public static Faction getFactionAt(Player location) {
+		return HCFactions.getInstance().getFactionManager().getFactionAt(location.getLocation());
+	}
+	public static Faction getFactionAt(Location location) {
+		return HCFactions.getInstance().getFactionManager().getFactionAt(location);
+	}
 
+	 public static Faction getFaction(String name) {
+		 return HCFactions.getInstance().getFactionManager().getFaction(name);
+	 }
 
+	 public static boolean isDeathban(Faction faction){
+		 return faction.isDeathban();
+	 }
 
+	 public static Relation getRelation(Faction faction, Player player){
+		return faction.getRelation(player);
+	 }
 
 }
