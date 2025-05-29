d8211557130a56fbcef7a87b8631c9ecd0ab8ffa
FANDESAWHOO <hcfpluginsv2@gmail.com>
2025-05-29 03:04:54 -0300

    changed positions

--- src/main/java/org/hcgames/hcfactions/command/FactionCommand.java
+++ src/main/java/org/hcgames/hcfactions/command/FactionCommand.java
@@ -1,14 +1,9 @@
 package org.hcgames.hcfactions.command;
 
 import lombok.Getter;
-import org.hcgames.hcfactions.HCFactions;
-import org.mineacademy.fo.Common;
-import org.mineacademy.fo.ReflectionUtil;
-import org.mineacademy.fo.Valid;
 import org.mineacademy.fo.annotation.AutoRegister;
 import org.mineacademy.fo.command.SimpleCommand;
 
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +24,6 @@ public final class FactionCommand extends SimpleCommand {
 
 	public FactionCommand() {
 		super("faction | fac | f");
-		registerFactionSubcommands(FactionSubCommand.class);
 	}
 
 	protected FactionSubCommand getById(String args){
@@ -39,21 +33,7 @@ public final class FactionCommand extends SimpleCommand {
 		return null;
 	}
 
-	protected final void registerFactionSubcommands(Class<? extends FactionSubCommand> parentClass) {
-		for (Class<? extends FactionSubCommand> clazz : ReflectionUtil.getClasses(HCFactions.getInstance(), parentClass)) {
-			if (Modifier.isAbstract(clazz.getModifiers()))
-				continue;
 
-			Valid.checkBoolean(Modifier.isFinal(clazz.getModifiers()), "Make child of " + parentClass.getSimpleName() + " class " + clazz.getSimpleName() + " final to auto register it!");
-
-			try {
-				FactionSubCommand subCommand = ReflectionUtil.instantiate(clazz);
-				subCommand.addArgument(); // <- LLÁMALO AQUÍ, una vez FactionCommand ya está instanciado
-			} catch (Throwable t) {
-				Common.error(t, "Failed to register subcommand: " + clazz.getSimpleName());
-			}
-		}
-	}
 
 	/**
 	 * Executed when the command is run. You can get the variables sender and args directly,
